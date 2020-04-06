9d0624f815bda198a8a05be0956e6fe1595b8ddd
Valmor F. de Almeida <dealmeidavf@gmail.com>
2020-04-06 12:46:48 -0400

    Updates to NB 12

--- notebooks/chen_3170/help.py
+++ notebooks/chen_3170/help.py
@@ -341,6 +341,20 @@ def plot_arrhenius_experimental_data( temp, k_cte ):
     return
 #*********************************************************************************
 def color_map( num_colors ):
+    '''
+    Nice colormap for plotting.
+
+    Parameters
+    ----------
+    num_colors: int, required
+        Number of colors.
+
+    Returns
+    -------
+    color_map: list(tuple(R,G,B,A))
+        List with colors interpolated from internal list of primary colors.
+
+    '''
 
     assert num_colors >= 1
 
@@ -359,7 +373,7 @@ def color_map( num_colors ):
 
     # order the primary colors here
     color_map = list()
-    color_map = [ red, blue, orange, magenta, green, yellow, black, cyan]
+    color_map = [red, blue, orange, magenta, green, yellow, cyan, black]
 
     num_primary_colors = len(color_map)
 
@@ -379,3 +393,74 @@ def color_map( num_colors ):
                 break
 
     return color_map
+#*********************************************************************************
+def get_covid_19_data( type='deaths' ):
+    '''
+    Load COVID-19 pandemic data from: https://github.com/CSSEGISandData/COVID-19
+
+    Parameters
+    ----------
+    type:  str, optional
+            type of data. Deaths ('deahts') and confirmed cases ('confirmed').
+            Default: 'deaths'.
+
+    Returns
+    -------
+    data: tuple(int, list(str), list(int))
+           (population, dates, data)
+
+    '''
+
+    import pandas as pd
+
+    if type == 'deaths':
+        df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv')
+        #df.to_html('covid_19_deaths.html')
+
+    elif type == 'confirmed':
+        df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
+        df_pop = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv')
+        #df.to_html('covid_19_deaths.html')
+        #df.to_html('covid_19_confirmed.html')
+
+    else:
+        assert True, 'invalid query type: %r (valid: "deaths", "confirmed"'%(type)
+
+    df = df.drop(['UID','iso2','iso3','Combined_Key','code3','FIPS','Lat', 'Long_','Country_Region'],axis=1)
+    df = df.rename(columns={'Province_State':'state/province','Admin2':'city'})
+
+    import numpy as np
+
+    state_names = list()
+
+    state_names_tmp = list()
+
+    for (i,istate) in enumerate(df['state/province']):
+        if istate.strip() == 'Wyoming' and df.loc[i,'city']=='Weston':
+            break
+        state_names_tmp.append(istate)
+
+    state_names_set = set(state_names_tmp)
+
+    state_names = list(state_names_set)
+    state_names = sorted(state_names)
+
+    dates = np.array(list(df.columns[3:]))
+
+    population = [0]*len(state_names)
+    cases = np.zeros( (len(df.columns[3:]),len(state_names)), dtype=np.float64)
+
+    for (i,istate) in enumerate(df['state/province']):
+        if istate.strip() == 'Wyoming' and df.loc[i,'city']=='Weston':
+            break
+
+        state_id = state_names.index(istate)
+        if type == 'confirmed':
+            population[state_id] += int(df_pop.loc[i,'Population'])
+        else:
+            population[state_id] += int(df.loc[i,'Population'])
+
+        cases[:,state_id] += np.array(list(df.loc[i, df.columns[3:]]))
+
+    return ( state_names, population, dates, cases )
+#*********************************************************************************
